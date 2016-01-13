7dc29ae5cb38056a3692d1cd1c3d6d3db786f330
Markusernst <markusernst92@web.de>
2016-01-13 10:32:51 +0100

    service my app added

---
+++ MS5/Prototypen_UI/MyApplication/app/src/main/java/com/example/user/myapplication/MapsActivity.java
@@ -0,0 +1,111 @@
+package com.example.user.myapplication;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Deutschland and move the camera
+        LatLng deutschland = new LatLng(50.93645868636, 6.9616086266323);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(deutschland));
+    }
+
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    public void onSearch(View view) {
+        EditText location_tf = (EditText)findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList = null;
+        if(location != null || !location.equals("")){
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location , 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude() , address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
+
+
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(50.938961923069, 6.9833329548588)).title("LANXESS arena 1"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(50.93645868636, 6.9616086266323)).title("Heumarkt"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(50.941262583518, 6.9408824051627)).title("Gerling Ring Karree"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(50.939818401849, 6.8841564926459)).title("Stadion P1"));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(50.938736474695, 6.9802124254481)).title("LANXESS arena 2"));
+        mMap.setMyLocationEnabled(true);
+
+
+        /*
+        Location location = new Location();
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+        mMap.animateCamera(cameraUpdate);
+         */
+    }
+
+}
