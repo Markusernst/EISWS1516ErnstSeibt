602ca27bb0194eefdd7c8c7202e4a026cafe3773
Markusernst <markusernst92@web.de>
2016-01-15 21:25:21 +0100

    added parkleitsystem und anwendungslogik
    + implementierungspräsentation
    + projektplan
    + systemarchitektur

--- MS5/Prototypen_UI/MyApplication/app/src/main/java/com/example/user/myapplication/MapsActivity.java
+++ MS5/Prototypen_UI/MyApplication/app/src/main/java/com/example/user/myapplication/MapsActivity.java
@@ -1,14 +1,10 @@
 package com.example.user.myapplication;
 
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,27 +15,28 @@ import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
-import com.squareup.okhttp.ResponseBody;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     OkHttp okHttp = new OkHttp();
+    String url = "http://192.168.43.158:8888/parking/";
+    LatLng latLng;
     public String resp;
     public Double lat;
     public Double lng;
     public String addresse;
     public String searchAddress;
+    List<LatLng> markerSmartpark = new ArrayList<LatLng>();
+    List<String> addressSmartpark = new ArrayList<String>();
+    List<LatLng> markerSart = new ArrayList<LatLng>();
+    List<String> addressStart = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +45,121 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setupParkButton();
+
+        try {
+            Call get = okHttp.doGetRequest(url, new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    resp = response.body().string();
+                    System.out.println(resp);
+                    try {
+                        JSONObject jsonObject = new JSONObject(resp);
+                        JSONArray jsonArray = jsonObject.getJSONArray("parking");
+                        System.out.println(jsonArray);
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject obj = jsonArray.getJSONObject(i);
+                            addresse = obj.getString("addresse");
+                            JSONObject geoData = obj.getJSONObject("geometry");
+                            lat = geoData.getDouble("latitude");
+                            lng = geoData.getDouble("longitude");
+                            System.out.println("latitude: " + lat + "longitude: " + lng);
+                            markerSart.add(new LatLng(lat, lng));
+                            addressStart.add(addresse);
+                        }
+                        runStartThread();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void runStartThread() {
+        runOnUiThread(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < markerSart.size(); i++) {
+                    mMap.addMarker(new MarkerOptions().position(markerSart.get(i)).title("Addresse: " + addressStart.get(i)));
+                }}
+        }));
+    }
+
+    private void setupParkButton() {
+
+        Button parkButton = (Button) findViewById(R.id.Smartpark);
+        parkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mMap.clear();
+
+                try {
+                    Call get = okHttp.doGetRequest(url, new Callback() {
+                        @Override
+                        public void onFailure(Request request, IOException e) {
+                        }
+
+                        @Override
+                        public void onResponse(Response response) throws IOException {
+                            resp = response.body().string();
+                            System.out.println(resp);
+                            try {
+                                JSONObject jsonObject = new JSONObject(resp);
+                                JSONArray jsonArray = jsonObject.getJSONArray("parking");
+                                System.out.println(jsonArray);
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject obj = jsonArray.getJSONObject(i);
+                                    Double tmpBelegung = obj.getDouble("belegung");
+                                    Double tmpKapazitaet = obj.getDouble("kapazitaet");
+                                    System.out.println(tmpBelegung);
+                                    System.out.println(tmpKapazitaet);
+                                    Double auslastung = tmpBelegung / tmpKapazitaet * 100;
+                                    System.out.println(auslastung);
+                                    Double auslastungsGrenze = 80.0;
+                                    System.out.println(i);
+                                    if (auslastung <= auslastungsGrenze) {
+                                        addresse = obj.getString("addresse");
+                                        System.out.println(addresse);
+                                        JSONObject geoData = obj.getJSONObject("geometry");
+                                        lat = geoData.getDouble("latitude");
+                                        lng = geoData.getDouble("longitude");
+                                        System.out.println("latitude: " + lat + "longitude: " + lng);
+                                        markerSmartpark.add(new LatLng(lat, lng));
+                                        addressSmartpark.add(addresse);
+                                    }
+
+                                }
+
+                                runParkThread();
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+   private void runParkThread() {
+        runOnUiThread(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < markerSmartpark.size(); i++) {
+                mMap.addMarker(new MarkerOptions().position(markerSmartpark.get(i)).title("Addresse: " + addressSmartpark.get(i)));
+            }}
+        }));
     }
 
 
@@ -76,11 +188,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void onSearch(View view) throws JSONException {
-
+        mMap.clear();
         EditText location_tf = (EditText)findViewById(R.id.TFaddress);
         searchAddress = location_tf.getText().toString();
-
-        String url = "http://192.168.43.158:8888/parking/";
         try {
             Call get = okHttp.doGetRequest(url, new Callback() {
                 @Override
@@ -90,23 +200,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 @Override
                 public void onResponse(Response response) throws IOException {
                     resp = response.body().string();
-                    //System.out.println(resp);
+                    System.out.println(resp);
                     try {
                         JSONObject jsonObject = new JSONObject(resp);
                         JSONArray jsonArray = jsonObject.getJSONArray("parking");
-
+                        System.out.println(jsonArray);
                         for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject obj = jsonArray.getJSONObject(i);
                         System.out.println(obj);
                         String tmpString =  obj.getString("addresse");
-                        //System.out.println(searchAddress);
-                        //System.out.println(tmpString);
+                        System.out.println(searchAddress);
+                        System.out.println(tmpString);
                             if(tmpString.equals(searchAddress)){
                                 addresse = tmpString;
                                 JSONObject geoData = obj.getJSONObject("geometry");
                                 lat = geoData.getDouble("latitude");
                                 lng = geoData.getDouble("longitude");
-                                //System.out.println("latitude: " + lat + "longitude: " + lng);
+                                System.out.println("latitude: " + lat + "longitude: " + lng);
                             }
 
                         }
@@ -127,9 +237,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         runOnUiThread(new Thread(new Runnable() {
             @Override
             public void run() {
-                LatLng latLng = new LatLng(lat,lng);
+                latLng = new LatLng(lat,lng);
                 mMap.addMarker(new MarkerOptions().position(latLng).title("Addresse: " + addresse));
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         }
     }));
     }
@@ -160,4 +269,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
          */
     }
 
+    public void onClick(View view) {
+    }
 }
